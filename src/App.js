2e281b85afad737de8b9b9707d49409158a9fd40
David Krasniy <davidkrasniy@gmail.com>
2022-02-02 19:15:27 -0800

    remove href

--- src/App.js
+++ src/App.js
@@ -63,14 +63,7 @@ function App() {
 
  
 
-            <div className="flex items-center justify-between">
-
-              <div className="text-sm">
-                <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
+           
 
             <div>
               <button
