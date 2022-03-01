e044a3776bdb042af310b29b4b5a96e731c4e286
Michael Vizcarra <50968946+mvizcarra@users.noreply.github.com>
2022-02-28 22:26:05 -0800

    Ability to create folder #MC-34 #MC-41

--- src/views/createNew.js
+++ src/views/createNew.js
@@ -66,7 +66,7 @@ function CreateNew() {
           className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-xl placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
         />
 
-        <Button type="submit" loading={creatingFolder}> Create Folder </Button>
+        <Button className="mt-4" type="submit" loading={creatingFolder}> Create Folder </Button>
       </form> 
     </div> 
 
