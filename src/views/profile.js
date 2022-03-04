2b63ce8062f82bb9cdc99a7a945e92f2f94c95f6
David Krasniy <davidkrasniy@gmail.com>
2022-03-04 12:28:17 -0800

    update my profile page

--- src/views/profile.js
+++ src/views/profile.js
@@ -19,20 +19,26 @@ function Profile() {
        
       </div>
      
-      <div className="bg-gray-50 rounded-xl p-8 my-6">
+      <div className="bg-gray-50 max-w-xl mx-auto rounded-xl p-8 my-6">
     
       <div className="space-y-4">
-       <b>Your Account Information</b> 
+       <b className="text-2xl text-center block">Your Account Information</b> 
+
+        <div className="h-32 w-32 bg-green-100 rounded-full flex items-center justify-center text-7xl mx-auto">🏄‍♂️</div>
+        <p className="text-4xl font-semibold text-center">@<b>{user.username}</b></p>
+        <div className="grid grid-cols-3 gap-4 text-left">
+        <p><span className="text-gray-500 block">First Name</span>{user.firstName}</p>
+        <p><span className="text-gray-500 block">Last Name</span>{user.lastName}</p>
+        <p><span className="text-gray-500 block">Email</span>{user.email}</p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 text-left">
+        <p><span className="text-gray-500 block">Date Joined: </span>{user.dateJoined}</p>
+        <p><span className="text-gray-500 block">User ID </span>{user.id}</p>
+        </div>
+       
 
        
-        <p>Username: <b>{user.username}</b></p>
-        <p>Email: {user.email}</p>
-        <p>First Name: {user.firstName}</p>
-        <p>Last Name: {user.lastName}</p>
-        <p>Date Joined: <b>{user.dateJoined}</b></p>
-
- 
-
+        
         </div>
       </div> 
 
