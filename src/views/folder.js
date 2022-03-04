b9519b108c0b51e71103004af2ce2e76d19c2e56
Michael Vizcarra <50968946+mvizcarra@users.noreply.github.com>
2022-03-03 17:27:58 -0800

    MC-43 Add dropzone markup

--- src/views/folder.js
+++ src/views/folder.js
@@ -6,49 +6,63 @@ import FolderList from "../components/FolderList";
 import { Routes, Link, useParams } from "react-router-dom";
 import { Spinner } from "../components/atoms/Spinner";
 import Layout from "../components/Layout";
+import { useDropzone } from "react-dropzone";
 
-
-function Folder() {
-  
-
+function Folder(props) {
   const { config } = useContext(AuthContext);
 
   let params = useParams();
 
-  const [currentFolder, setCurrentFolder] = useState({name: "-", id: ""})
-  const [loadingFolder, setLoadingFolder] = useState(true)
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
+  const [currentFolder, setCurrentFolder] = useState({ name: "-", id: "" });
+  const [loadingFolder, setLoadingFolder] = useState(true);
+
+  const files = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
   useEffect(() => {
     axiosInstance
       // path, data, config
       .get(`/folder/${parseInt(params.folderId)}`, config)
       .then((r) => {
-        console.log(r.data.data)
-        setCurrentFolder(r.data.data)
-        setLoadingFolder(false)
+        console.log(r.data.data);
+        setCurrentFolder(r.data.data);
+        setLoadingFolder(false);
       })
       .catch((e) => {
-        setLoadingFolder(false)
-      })
+        setLoadingFolder(false);
+      });
   }, []);
-
- 
 
   return (
     <Layout>
-    <div className="max-w-4xl mx-auto my-6 px-4 md:px-6">
-      <Link className="block" to={'/dashboard'}>Back</Link>
-      <b className="text-xl"> {currentFolder.name}</b> 
-      
-      <div className="bg-gray-50 rounded-xl p-8 my-6">
-      {loadingFolder ? <Spinner/> : null}
+      <div className="max-w-4xl mx-auto my-6 px-4 md:px-6">
+        <Link className="block" to={"/dashboard"}>
+          Back
+        </Link>
+        <b className="text-xl"> {currentFolder.name}</b>
 
-      Folder ID:  {params.folderId}
-     
-       
-      </div> 
+        <div className="bg-gray-50 rounded-xl p-8 my-6">
+          {loadingFolder ? <Spinner /> : null}
+          Folder ID: {params.folderId}
 
-    </div>
+          <section className="container">
+          <div {...getRootProps({ className: "dropzone border p-4 rounded border-gray-400" })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+          <aside>
+            <h4>Files</h4>
+            <ul>{files}</ul>
+          </aside>
+        </section>
+        </div>
+
+        
+      </div>
     </Layout>
   );
 }
