1b1d03bf8485ea8a24fbf86657d1a81edf598a34
David Krasniy <davidkrasniy@gmail.com>
2022-03-03 20:01:46 -0800

    #mc-42 add modal, edit button to folder header

---
+++ src/components/EditFolderDetails.js
@@ -0,0 +1,72 @@
+
+
+import React, { Fragment, useState } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import Button from "../components/atoms/Button";
+
+export default function EditFolderDetails({ folderId, folderData }) {
+
+    let [isOpen, setIsOpen] = useState(false)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // @TODO
+        console.log("submit the update vbalues to the edit folder service.,..")
+
+        // it should also provide the updated info back to the folder page and update the header title/desc etc.. 
+    }
+
+
+
+    return (
+        <><Button color="secondary" onClick={() => setIsOpen(true)}>Edit Folder</Button>
+
+            <Transition show={isOpen} as={Fragment}>
+                <Dialog open={isOpen} onClose={() => setIsOpen(false)}
+                    className="fixed z-10 inset-0 overflow-y-auto"
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+
+                >
+                    <div className="flex items-center justify-center min-h-screen">
+
+                        <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+
+                            <form onSubmit={handleSubmit} className="relative bg-white rounded max-w-xl mx-auto p-8 rounded-xl">
+                                <Dialog.Title className={"font-bold text-lg"}>Edit Folder Id {folderId}</Dialog.Title>
+                                <Dialog.Description>
+                                    Edit the folder by updating the fields below  Edit the folder by updating the fields below  Edit the folder by updating the fields below
+                                </Dialog.Description>
+ 
+                                <div className="flex mt-8 justify-end">
+                                    <Button onClick={() => setIsOpen(false)} color="secondary">Cancel</Button>
+                                    <Button type="submit" onClick={() => setIsOpen(false)} color="primary" className="ml-2">Save Changes</Button>
+                                </div>
+                            </form>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition>
+
+        </>
+    )
+
+
+
+}
--- src/views/folder.js
+++ src/views/folder.js
@@ -1,14 +1,13 @@
-import React, { useContext, useState, useEffect } from "react";
-import Button from "../components/atoms/Button";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { AuthContext } from "../components/AuthProvider";
 import { axiosInstance } from "../client";
-import FolderList from "../components/FolderList";
 import { Routes, Link, useParams, Route } from "react-router-dom";
 import { Spinner } from "../components/atoms/Spinner";
 import Layout from "../components/Layout";
 import { useDropzone } from "react-dropzone";
 import RouteSlip from '../components/RouteSlip'
-
+import Button from "../components/atoms/Button";
+import EditFolderDetails from '../components/EditFolderDetails'
 
 function Folder(props) {
   const { config } = useContext(AuthContext);
@@ -18,17 +17,22 @@ function Folder(props) {
   const [currentFolder, setCurrentFolder] = useState({ name: "-", id: "" });
   const [loadingFolder, setLoadingFolder] = useState(true);
 
+  const onDrop = useCallback(acceptedFiles => {
+    console.log("Dropped some files", acceptedFiles)
+  }, [])
+
   const {
     acceptedFiles,
     fileRejections,
     getRootProps,
     getInputProps
   } = useDropzone({
-    accept: 'application/pdf, image/jpeg,image/png'
+    accept: 'application/pdf, image/jpeg,image/png',
+    onDrop
   });
 
   const acceptedFileItems = acceptedFiles.map(file => (
-    <li key={file.path}>
+    <li key={file.path} className="border-t py-3">
       {file.path} - {file.size} bytes
     </li>
   ));
@@ -61,48 +65,55 @@ function Folder(props) {
   return (
     <Layout>
       <div className="max-w-7xl mx-auto my-6 px-4 md:px-6">
-        <Link className="block" to={"/dashboard"}>
-          Back
-        </Link>
-        <b className="text-2xl my-2"> {currentFolder.name}</b>
-       <br/> Folder ID: {params.folderId}
+        <div className="flex justify-between items-center">
+
+          <div>
+            <Link className="block" to={"/dashboard"}>
+              Back
+            </Link>
+            <b className="text-2xl my-2"> {currentFolder.name}</b>
+            <br /> Folder ID: {params.folderId}
+          </div>
+          <div>
+            <EditFolderDetails folderId={params.folderId} />
+
+          
+          </div>
+
+        </div>
+
 
         <div className="my-6">
           {loadingFolder ? <Spinner /> : null}
-       
 
-          <section className="flex border-t">
-            <div className="w-full md:w-1/3 xl:w-1/4 bg-white py-6 px-2 border-r">
-            <RouteSlip/>
+          <section className="flex border-t flex-wrap">
+            <div className="w-full md:w-1/3 xl:w-1/4 bg-white py-6 px-2 md:border-r">
+              <RouteSlip />
             </div>
-            <div className="w-full md:w-2/3 xl:w-3/4 p-8 ">
-            <h3 className="font-semibold">Folder Contents</h3>
+            <div className="w-full md:w-2/3 xl:w-3/4 px-2 md:p-8">
+              <h3 className="font-semibold">Folder Contents</h3>
 
-         
-          <button {...getRootProps({ className: 'dropzone' })}  className="my-4 bg-gray-50 p-8 rounded-xl w-full">
-              <input {...getInputProps()}  className="bg-red-500 p-8 block"/>
-              <p>Drag 'n' drop some files here, or click to select files</p>
-              <em>(Only *.jpeg and *.png images will be accepted)</em>
+              <button {...getRootProps({ className: 'dropzone' })} className="my-4 bg-gray-50 p-8 rounded-xl w-full overflow-hidden">
+                <input {...getInputProps()} className="bg-red-500 p-8 block" />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+                <em>(Only *.jpeg and *.png images will be accepted)</em>
 
-              <div className="bg-gray-100 -mb-8 -mx-8 rounded-b-xl px-8  mt-8">
-               
-              <ul>{acceptedFileItems}</ul>
+                <div className="bg-gray-100 -mb-8 -mx-8 rounded-b-xl px-8  mt-8">
+                  <ul className="py-8 text-left">{acceptedFileItems}</ul>
+                  {/* 
               {acceptedFileItems.length > 0 ? <div className="my-2"> <h4 className="mb-2"><b>Accepted files</b></h4>
               <ul>{acceptedFileItems}</ul></div> : null}
 
               {fileRejectionItems.length > 0 ? <div className="my-2"> <h4 className="mb-2"><b>Rejected files</b></h4>
-              <ul>{fileRejectionItems}</ul></div> : null}
-             
-            
+              <ul>{fileRejectionItems}</ul></div> : null} */}
+                </div>
+              </button>
 
-              </div>
-            </button>
-           
-          </div>
-        </section>
+            </div>
+          </section>
         </div>
 
-        
+
       </div>
     </Layout>
   );
