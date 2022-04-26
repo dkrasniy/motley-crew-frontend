9dad1b818633b1a33356e01d655f5b318570482f
Jose <jose.uriel.torres5@gmail.com>
2022-04-25 22:12:44 -0700

    Login Testing Updates

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -4,15 +4,29 @@ import "@testing-library/jest-dom/extend-expect";
 import { MemoryRouter } from "react-router-dom";
 import { Login } from "../views/login";
 import { act } from "react-test-renderer";
+import App from '../App'
 
 test ("logging in using credentials", () => {
-    const root = document.createElement('div');
-    document.body.appendChild(root);
+
+    const root = document.createElement('div')
+    document.body.appendChild(root)
+    
     render(
-        <MemoryRouter initialEntries={['/dashboard']}>
-            
+        <MemoryRouter initialEntries={['/login']}>
+          <App />
         </MemoryRouter>,
         root
       );
-    expect(document.getElementById('test')).toBeDefined;
+
+      act(() => {
+        const emailInput = document.getElementById("email")
+        emailInput.value = "ecovertmc"
+        const passInput = document.getElementById("password")
+        passInput.value = "ecovertmc"
+        const signIn = document.querySelectorAll('input[type=submit]')
+        signIn.dispatchEvent(new MouseEvent("click"))
+      })
+
+    expect(document.querySelectorAll('header')).toBeDefined;
+
 })
