63ff4ad36960d2f8706450212e611e5d160c6a20
Jose Torres <73760867+JoseUTorres@users.noreply.github.com>
2022-05-13 12:47:06 -0700

    more test specs

--- cypress/integration/folder.spec.js
+++ cypress/integration/folder.spec.js
@@ -14,9 +14,31 @@ describe('test folder functions', () => {
         // Return home and assert new folder was created
         cy.findByRole('link', {name: /home/i}).click()
         cy.findByText(/test folder creation/i).should('be.visible')
+    })
+
+    it ('edit folder', () => {
+        //Login
+        cy.visit('https://motleycrewdev.com/login')
+        cy.findByRole('button', {name: /sign in/i}).click()
+        // Edit folder
+        cy.findByText(/test folder creation/i).click()
+        cy.findByRole('button', {  name: /edit folder/i}).click()
+        cy.findByRole('textbox', {  name: /folder name/i}).clear()
+        cy.findByRole('textbox', {  name: /folder name/i}).type('Test Folder Edit')
+        cy.findByRole('button', {  name: /save changes/i}).click()
+        // Assert edit to folder
+        cy.findByText(/test folder edit/i).should('be.visible')
+    })
+
+    it ('delete folder', () => {
+        // Login
+        cy.visit('https://motleycrewdev.com/login')
+        cy.findByRole('button', {name: /sign in/i}).click()
         // Remove test folder creation
         cy.findByText(/test folder creation/i).click()
         cy.findByRole('button', {name: /more/i}).click()
         cy.findByRole('button', {name: /delete/i}).click()
+        // Assert that folder was deleted
+        cy.findByText(/test folder edit/i).should('not.exist')
     })
 })
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,25 @@
-describe('login', () => {
+describe('tests related to authetication/login', () => {
     it ('user can login', () => {
+        // navigate to site and login
         cy.visit('https://motleycrewdev.com/login')
         // cy.findByRole('textbox', {name: /Email address/i}).clear()
         // cy.findByRole('textbox', {name: /Email address/i}).type('ecovertmc')
         // cy.findByLabelText(/password/i).clear()
         // cy.findByLabelText(/password/i).type('s4!apgjuadg5')
         cy.findByRole('button', {name: /sign in/i}).click()
+        // assert that the user has logged in by checking that the home navbar is viewable
         cy.findByRole('link', {name: /home/i}).should('be.visible')
     })
+
+    it ('user can logout', () => {
+        // navigate to site and login
+        cy.visit('https://motleycrewdev.com/login')
+        cy.findByRole('button', {name: /sign in/i}).click()
+        // click on user profile and logout
+        cy.findByRole('button', {  name: /🤦 ecovertmc ethancovert@csus\.edu/i}).click()
+        cy.findByRole('menuitem', {  name: /log out/i}).click()
+        // assert that the user has logged out by checking if the login screen is viewable
+        cy.findByRole('heading', {  name: /welcome/i}).should('be.visible')
+    })
+
 })
